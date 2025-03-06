af523263e9177613fe12106fb76fdaaf14c3e6bd
ryota <r.komatsu.24.10.1600@4nonome.com>
2025-03-06 11:44:07 +0900

    ログイン機能の実装

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+from django.urls import reverse_lazy
+
 AUTH_USER_MODEL = "accounts.User"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,3 +128,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_URL = reverse_lazy("accounts:login")
+LOGIN_REDIRECT_URL = reverse_lazy("tweets:home")
