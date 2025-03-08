40c3e97bed836f319edaaf40f817a233a49be3de
ryota <r.komatsu.24.10.1600@4nonome.com>
2025-03-08 13:20:41 +0900

    ログアウト機能の実装

--- accounts/urls.py
+++ accounts/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
         ),
         name="login",
     ),
-    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("<str:username>/", views.UserProfileView.as_view(), name="user_profile"),
     # path('<str:username>/follow/', views.FollowView.as_view(), name='follow'),
     # path('<str:username>/unfollow/', views.UnFollowView, name='unfollow'),
